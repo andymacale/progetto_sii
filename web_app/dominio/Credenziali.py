d376ce96176f899e6ff0fc17bb94d857ef66f11f
andymacale <and.macale@stud.uniroma3.it>
2026-02-25 16:52:37 +0100

    piccola correzione

--- web_app/dominio/Credenziali.py
+++ web_app/dominio/Credenziali.py
@@ -3,6 +3,3 @@ class Credenziali:
     def __init__(self, email: str, password: str):
         self.email = email
         self.password = password
-
-    def check_password(self, inserita: str) -> bool:
-        return self.password == inserita
