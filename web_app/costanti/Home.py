4a22915f04e8057c21fcd360905817f004376873
andymacale <and.macale@stud.uniroma3.it>
2026-03-09 21:24:51 +0100

    variabile ambiente

--- web_app/costanti/Home.py
+++ web_app/costanti/Home.py
@@ -3,3 +3,4 @@ import os
 class Home:   
     MEDICAL_HOME = os.environ.get("MEDICAL_HOME")
     GRAFICA = os.path.join(MEDICAL_HOME, "web_app", "grafica")
+    DATASET = os.path.join(MEDICAL_HOME, "data")
